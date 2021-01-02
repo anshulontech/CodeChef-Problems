847580f427d2b9e444e1f77bf92f3e1993547765
anshul7sh <anshul7sh@gmail.com>
2021-01-02 15:45:50 +0530

    Template Updated

--- Templates/C++_Template.cpp
+++ Templates/C++_Template.cpp
@@ -1,15 +1,20 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
 
-int main() {
-    int t;
-    cin>>t;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long t;
+    cin >> t;
 
-    while(t--) {
-        int n;
-        cin>>n;
-
-        helper();
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        
     }
 
     return 0;
